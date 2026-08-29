73ed0e06a5be80b26ffbeddd5f86960d22432bb5
Alexandra Tropina <alexandratropina@mail.ru>
2026-08-30 00:14:51 +0700

    1 этап диплома - исправления по checkstyle #2

--- stats-service/stats-inner-service/src/main/java/explore/ExploreStatistics.java
+++ stats-service/stats-inner-service/src/main/java/explore/ExploreStatistics.java
@@ -5,5 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExploreStatistics {
-    public static void main(String[] args) {SpringApplication.run(ExploreStatistics.class, args); }
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreStatistics.class, args);
+    }
 }
